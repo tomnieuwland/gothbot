cf892cff0c8438db3a74de18fb218c29c9d4df24
Tom Nieuwland <thomasnieuwland19@gmail.com>
2021-02-11 01:05:38 +1100

    Moved to environment variables (loaded from .env when available). Also added requirements.txt (#5)

--- gothbot/run.py
+++ gothbot/run.py
@@ -1,7 +1,8 @@
 import argparse
 import logging
+import os
 
-from dotenv import dotenv_values
+from dotenv import load_dotenv
 
 from bot import GothBot
 
@@ -12,9 +13,10 @@ from command_modules.minecraft import MinecraftCommandModule
 logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
-    config = dotenv_values("../.env")
+    # Loads environment variables from .env in root directory where available
+    load_dotenv("../.env")
 
-    if config.get("verbose"):
+    if os.environ.get("verbose"):
         logging.basicConfig(level=logging.DEBUG)
     else:
         logging.basicConfig(level=logging.INFO)
@@ -23,18 +25,18 @@ if __name__ == "__main__":
 
     command_modules = []
 
-    if config.get("MINECRAFT_HOST") and config.get("MINECRAFT_PORT"):
+    if os.environ.get("MINECRAFT_HOST") and os.environ.get("MINECRAFT_PORT"):
         command_modules.append(
             MinecraftCommandModule(
-                minecraft_host=config.get("MINECRAFT_HOST"),
-                minecraft_port=config.get("MINECRAFT_PORT"),
-                command_prefix=config["COMMAND_PREFIX"],
+                minecraft_host=os.environ.get("MINECRAFT_HOST"),
+                minecraft_port=os.environ.get("MINECRAFT_PORT"),
+                command_prefix=os.environ["COMMAND_PREFIX"],
             )
         )
     else:
         logger.warning("Missing minecraft info, not initialising")
 
-    client = GothBot(command_prefix=config["COMMAND_PREFIX"])
+    client = GothBot(command_prefix=os.environ["COMMAND_PREFIX"])
 
     for handler_instance in special_handlers:
         client.register_special_handler(handler_instance)
@@ -42,4 +44,4 @@ if __name__ == "__main__":
     for command_module_instance in command_modules:
         client.register_command_module(command_module_instance)
 
-    client.run(config["DISCORD_TOKEN"])
+    client.run(os.environ["DISCORD_TOKEN"])
